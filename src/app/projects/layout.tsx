47560d970bdee2242e9905880e4a2ff39903772b
anas <you@example.com>
2025-10-29 13:27:56 +0000

    !

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,5 +1,11 @@
 import "../(root)/globals.css";
+import type { Metadata } from "next";
 
+
+export const metadata: Metadata = {
+  title: "My Projects",
+  description: "You can find here all my projects",
+};
 
 export default function RootLayout({
   children,
